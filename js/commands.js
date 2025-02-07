2d2f6c34a2f12015710baa28c9cb74a8c35e3c72
Michael Wood <mike@michaelwood.com>
2025-02-07 14:08:02 -0800

    Re-use commands.

--- js/commands.js
+++ js/commands.js
@@ -17,14 +17,15 @@ class TerminalCommand extends Command {
     execute(terminal) {
         // Create terminal process launcher
         const createTerminal = (container) => {
-            // Create new terminal instance with same commands
-            const newTerminal = new Terminal(container, availableCommands);
+            // Create new terminal instance
+            const newTerminal = new Terminal(container);
             newTerminal.processManager = terminal.processManager;
             return newTerminal;
         };
 
-        // Launch new terminal process
-        terminal.processManager.launchProcess(createTerminal);
+        // Launch new terminal process with current process as parent
+        const currentProcessId = terminal.processManager.getActiveProcessId();
+        terminal.processManager.launchProcess(createTerminal, currentProcessId);
         return "New terminal process created.";
     }
 }
--- js/terminal/Terminal.js
+++ js/terminal/Terminal.js
@@ -1,36 +1,7 @@
 import { TerminalUI } from './TerminalUI.js';
 import { CommandHistory } from './CommandHistory.js';
 import { CommandProcessor } from './CommandProcessor.js';
-import MotdCommand from '../commands/motd.js';
-import UptimeCommand from '../commands/uptime.js';
-import WhoamiCommand from '../commands/whoami.js';
-import PwdCommand from '../commands/pwd.js';
-import ExitCommand from '../commands/exit.js';
-import GitHubCommand from '../commands/github.js';
-import DateCommand from '../commands/date.js';
-import Command from '../commands/Command.js';
-
-// Define Terminal Command class inline
-class TerminalCommand extends Command {
-    constructor() {
-        super('terminal', 'Spawn a new terminal process');
-    }
-
-    execute(terminal) {
-        // Create terminal process launcher
-        const createTerminal = (container) => {
-            // Create new terminal instance with same commands
-            const newTerminal = new Terminal(container);
-            newTerminal.processManager = terminal.processManager;
-            return newTerminal;
-        };
-
-        // Launch new terminal process with current process as parent
-        const currentProcessId = terminal.processManager.getActiveProcessId();
-        terminal.processManager.launchProcess(createTerminal, currentProcessId);
-        return "New terminal process created.";
-    }
-}
+import { availableCommands } from '../commands.js';
 
 export class Terminal {
     constructor(container) {
@@ -42,40 +13,8 @@ export class Terminal {
         // Initialize command history
         this.history = new CommandHistory();
 
-        // Initialize commands
-        const commandInstances = {
-            "motd": new MotdCommand(),
-            "exit": new ExitCommand(),
-            "github": new GitHubCommand(),
-            "pwd": new PwdCommand(),
-            "whoami": new WhoamiCommand(),
-            "date": new DateCommand(),
-            "uptime": new UptimeCommand(),
-            "terminal": new TerminalCommand(),
-        };
-
-        // Create help command
-        const commands = {
-            ...commandInstances,
-            help: () => {
-                const cmds = Object.keys(commandInstances).sort();
-                let output = "Available commands:\n";
-                cmds.forEach(cmd => {
-                    const command = commandInstances[cmd];
-                    output += `  ${command.getName()} - ${command.getDescription()}\n`;
-                });
-                return output;
-            }
-        };
-
-        // Wrap each command's execute method
-        Object.keys(commandInstances).forEach(cmd => {
-            const command = commandInstances[cmd];
-            commands[cmd] = (terminal) => command.execute(terminal);
-        });
-
-        // Initialize command processor
-        this.processor = new CommandProcessor(commands);
+        // Initialize command processor with available commands
+        this.processor = new CommandProcessor(availableCommands);
 
         // Get process manager reference
         this.processManager = window.processManager;
